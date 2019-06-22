c0180c6419c2c0ab0ed450f316f896385d786a39
Ma'ayan Doron <maayan@Maayans-MacBook-Pro.local>
2019-06-22 19:45:32 -0400

    creating list entry

--- app/src/main/java/com/mador96/voom/TransportOptions.java
+++ app/src/main/java/com/mador96/voom/TransportOptions.java
@@ -2,6 +2,8 @@ package com.mador96.voom;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class TransportOptions extends AppCompatActivity {
 
@@ -17,6 +19,18 @@ public class TransportOptions extends AppCompatActivity {
         Double destinationLat = getIntent().getDoubleExtra("D_LAT", 0);
         Double destinationLng = getIntent().getDoubleExtra("D_LNG", 0);
         String passengers = getIntent().getStringExtra("PASSENGERS");
+
+        TextView originText = (TextView)findViewById(R.id.originTextView);
+        TextView destText = (TextView)findViewById(R.id.destTextView);
+
+        //Strip out only first part of address???
+
+        originText.setText(theOrigin);
+        destText.setText(theDestination);
+
+        //Initialize list for prices
+
+        ListView listView = (ListView)findViewById(R.id.list);
     }
 
 }
